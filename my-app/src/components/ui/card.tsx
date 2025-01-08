952262babc2d097a69e6c2f487300ae05e9353a3
manahil jutt <manahilshafaqatalijutt@gmail.com>
2025-01-08 18:39:43 +0500

    updated
    add my latest work

--- my-app/src/components/ui/card.tsx
+++ my-app/src/components/ui/card.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-import { cn } from "@/lib/utils"
+import { cn } from "npx create-next-app@latest <prefix>/lib/utils"
 
 const Card = React.forwardRef<
   HTMLDivElement,
